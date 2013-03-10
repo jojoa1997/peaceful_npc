23964b22ca5ef0388e1ffff43f198e5f80592aa1
jojoa1997 <jkanawalliii@earthlink.net>
2013-03-10 13:13:52 -0400

    allows all files to be used

--- init.lua
+++ init.lua
@@ -1,11 +1,11 @@
 --Loads other files
 dofile(minetest.get_modpath("peaceful_npc").."/npc/npc_def.lua")
 dofile(minetest.get_modpath("peaceful_npc").."/npc/npc_fast.lua")
---dofile(minetest.get_modpath("peaceful_npc").."/commands.lua")
+dofile(minetest.get_modpath("peaceful_npc").."/npc/npc_dwarf.lua")
+dofile(minetest.get_modpath("peaceful_npc").."/commands.lua")
 dofile(minetest.get_modpath("peaceful_npc").."/items.lua")
---dofile(minetest.get_modpath("peaceful_npc").."/spawning.lua")
-dofile(minetest.get_modpath("peaceful_npc").."/plants_lib.lua")
---dofile(minetest.get_modpath("peaceful_npc").."/recipes.lua")
+dofile(minetest.get_modpath("peaceful_npc").."/spawning.lua")
+dofile(minetest.get_modpath("peaceful_npc").."/recipes.lua")
 
 --NPC Privilege
 minetest.register_privilege("peacefulnpc", { description = "allows to use spawn command", give_to_singleplayer = true})
