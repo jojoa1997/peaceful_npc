54ee9027f4878a57f71e12bea91386b46e69d354
jojoa1997 <jkanawalliii@earthlink.net>
2013-03-10 13:26:45 -0400

    added dwarf stuff

--- items.lua
+++ items.lua
@@ -1,13 +1,13 @@
 --Spawn code
 function npc_spawner(pos, SPAWN_TYPE)
 	local MAX_NPC = 15
-	local count = table.getn(minetest.env:get_objects_inside_radius(pos, 100))
+	local count = table.getn(minetest.env:get_objects_inside_radius(pos, 50))
 	if count == nil then
 		count = 0
 	end
 	
 	if count <= MAX_NPC then
-		minetest.env:add_entity({x=pos.x+math.random(-1,1),y=pos.y+math.random(2,3),z=pos.z+math.random(-1,1)}, (SPAWN_TYPE))
+		minetest.env:add_entity({x=pos.x+math.random(-1,1),y=pos.y+math.random(2,3),z=pos.z+math.random(-1,1)}, SPAWN_TYPE)
 	end
 end
 
@@ -50,7 +50,7 @@ minetest.register_node("peaceful_npc:spawner_npc_def", {
 	sunlight_propagates = true,
 	paramtype = "light",
 	mesecons = {effector = {
-		action_on = npc_spawner
+		action_on = function(pos) npc_spawner(pos, "peaceful_npc:npc_def") end,
 	}}
 })
 minetest.register_abm({
@@ -97,19 +97,70 @@ minetest.register_node("peaceful_npc:spawner_npc_fast", {
 	drawtype = "glasslike",
 	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
 	sounds = default.node_sound_glass_defaults(),
-	tiles = {"peaceful_npc_spawner_def.png"},
+	tiles = {"peaceful_npc_spawner_fast.png"},
 	sunlight_propagates = true,
 	paramtype = "light",
 	mesecons = {effector = {
-		action_on = npc_spawner
+		action_on = function(pos) npc_spawner(pos, "peaceful_npc:npc_fast") end,
 	}}
 })
 minetest.register_abm({
 	nodenames = {"peaceful_npc:spawner_npc_fast"},
-	interval = 20,
+	interval = 30,
 	chance = 1,
 	action = function(pos)
 		npc_spawner(pos, "peaceful_npc:npc_fast")
+	end,
+})
+
+--Item Code for dwarf npcs
+minetest.register_node("peaceful_npc:summoner_npc_dwarf", {
+	description = "Dwarf NPC Summoner",
+	image = "peaceful_npc_npc_summoner_dwarf.png",
+	inventory_image = "peaceful_npc_npc_summoner_dwarf.png",
+	wield_image = "peaceful_npc_npc_summoner_dwarf.png",
+	paramtype = "light",
+	tiles = {"peaceful_npc_spawnegg.png"},
+	is_ground_content = true,
+	drawtype = "glasslike",
+	groups = {crumbly=3},
+	selection_box = {
+		type = "fixed",
+		fixed = {0,0,0,0,0,0}
+	},
+	sounds = default.node_sound_dirt_defaults(),
+	on_place = function(itemstack, placer, pointed)
+		local name = placer:get_player_name()
+		if (minetest.check_player_privs(name, {peacefulnpc=true})) then
+            pos = pointed.above
+            pos.y = pos.y + 1
+        minetest.env:add_entity(pointed.above,"peaceful_npc:npc_dwarf")
+        itemstack:take_item(1)
+	else
+		minetest.chat_send_player(name, "Nope! You need to have the peacefulnpc priv!")
+	end
+	return itemstack
+end	
+})
+
+minetest.register_node("peaceful_npc:spawner_npc_dwarf", {
+	description = "Dwarf NPC Portal",
+	drawtype = "glasslike",
+	groups = {snappy=1,choppy=2,oddly_breakable_by_hand=2,flammable=3,wood=1},
+	sounds = default.node_sound_glass_defaults(),
+	tiles = {"peaceful_npc_spawner_dwarf.png"},
+	sunlight_propagates = true,
+	paramtype = "light",
+	mesecons = {effector = {
+		action_on = function(pos) npc_spawner(pos, "peaceful_npc:npc_dwarf") end,
+	}}
+})
+minetest.register_abm({
+	nodenames = {"peaceful_npc:spawner_npc_dwarf"},
+	interval = 60,
+	chance = 1,
+	action = function(pos)
+		npc_spawner(pos, "peaceful_npc:npc_dwarf")
 	end,
 })
 
--- recipes.lua
+++ recipes.lua
@@ -1,41 +1,37 @@
 --
 --Crafts
 --
+function npc_recipes(spawn_type, summon_core)
+	minetest.register_craft({
+		output = 'peaceful_npc:spawner_npc_'..spawn_type,
+		recipe = {
+			{'default:mese_block', 'default:glass', 'default:mese_block'},
+			{'default:glass', 'peaceful_npc:summoner_npc_'..spawn_type, 'default:glass'},
+			{'default:mese_block', 'default:glass', 'default:mese_block'},
+		}
+	})
 
-minetest.register_craft({
-	output = 'peaceful_npc:npc_spawner',
-	recipe = {
-		{'default:mese_block', 'default:glass', 'default:mese_block'},
-		{'default:glass', 'default:mese_crystal', 'default:glass'},
-		{'default:mese_block', 'default:glass', 'default:mese_block'},
-	}
-})
+	minetest.register_craft({
+		output = 'peaceful_npc:summoner_npc_'..spawn_type,
+		recipe = {
+			{'default:mese_crystal', 'default:glass', 'default:mese_crystal'},
+			{'default:glass', summon_core, 'default:glass'},
+			{'default:mese_crystal', 'default:glass', 'default:mese_crystal'},
+		}
+	})
 
-minetest.register_craft({
-	output = 'peaceful_npc:summoner',
-	recipe = {
-		{'default:mese_crystal', 'default:glass', 'default:mese_crystal'},
-		{'default:glass', 'default:coal_lump', 'default:glass'},
-		{'default:mese_crystal', 'default:glass', 'default:mese_crystal'},
-	}
-})
+	minetest.register_craft({
+		output = 'peaceful_npc:summoner_npc_'..spawn_type,
+		recipe = {
+			{'default:mese', 'default:glass', 'default:mese'},
+			{'default:glass', summon_core, 'default:glass'},
+			{'default:mese', 'default:glass', 'default:mese'},
+		}
+	})
+end
 
-minetest.register_craft({
-	output = 'peaceful_npc:npc_spawner',
-	recipe = {
-		{'default:mese', 'default:glass', 'default:mese'},
-		{'default:glass', 'default:mese', 'default:glass'},
-		{'default:mese', 'default:glass', 'default:mese'},
-	}
-})
-
-minetest.register_craft({
-	output = 'peaceful_npc:summoner',
-	recipe = {
-		{'default:mese', 'default:glass', 'default:mese'},
-		{'default:glass', 'default:coal_lump', 'default:glass'},
-		{'default:mese', 'default:glass', 'default:mese'},
-	}
-})
+npc_recipes('def', 'default:steel_ingot')
+npc_recipes('fast', 'default:cactus')
+npc_recipes('dwarf', 'bucket:bucket_lava')
 
 print("Peaceful NPC recipes.lua loaded! By jojoa1997!")
